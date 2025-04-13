dd4e9ef2c1e14a88f6dce4778ff1ced29a8322be
Reem Mohsen <rmohsen@umich.edu>
2025-04-13 14:36:27 -0400

    Fix: remove EmailException from getHostName override

--- src/test/java/org/apache/commons/mail/EmailConcrete.java
+++ src/test/java/org/apache/commons/mail/EmailConcrete.java
@@ -23,7 +23,7 @@ public class EmailConcrete extends MultiPartEmail {
     }
 
     @Override
-    public String getHostName() throws EmailException {
+    public String getHostName() {
         String hostName = super.getHostName();
         if (hostName == null && getMailSession() != null) {
             return getMailSession().getProperty("mail.host");
